5b10536b7b6cbf5622495692aac0249be54bf838
Cedrick <52387527+Cpatterson23@users.noreply.github.com>
2022-04-04 19:52:51 -0500

    Add files via upload

---
+++ working with numbers/working with numbers/src/main.js
@@ -0,0 +1,14 @@
+let x = 4
+let y = 2
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x**y);
+
+console.log(x > y);
+console.log(x >= y);
+console.log(x < y);
+console.log(x <= y);
+console.log(x==y);
+console.log(x != y);
