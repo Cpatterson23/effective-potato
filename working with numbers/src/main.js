02d904495e1f0cf75dc38255b635a80573e2ad93
Cedrick <52387527+Cpatterson23@users.noreply.github.com>
2022-04-04 19:55:49 -0500

    Update main.js

--- working with numbers/src/main.js
+++ working with numbers/src/main.js
@@ -1,24 +1,14 @@
 let x = 4
 let y = 2
 console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x**y);
 
-a = 5
-b = 5
-
-console.log(a-b);
-
-let c = 1
-let d = 1
-
-console.log(c*d);
-
-
-let e = 10
-let f = 2
-
-console.log(e/f)
-
-let g = 12
-let h = 6
-
-console.log(g**h);
+console.log(x > y);
+console.log(x >= y);
+console.log(x < y);
+console.log(x <= y);
+console.log(x==y);
+console.log(x != y);
