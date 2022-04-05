279d67db5b4be74620d2f196d9ecdb17d29e1757
Cedrick <52387527+Cpatterson23@users.noreply.github.com>
2022-04-04 19:08:56 -0500

    Add files via upload

---
+++ working with numbers/src/main.js
@@ -0,0 +1,24 @@
+let x = 4
+let y = 2
+console.log(x+y);
+
+a = 5
+b = 5
+
+console.log(a-b);
+
+let c = 1
+let d = 1
+
+console.log(c*d);
+
+
+let e = 10
+let f = 2
+
+console.log(e/f)
+
+let g = 12
+let h = 6
+
+console.log(g**h);
